94fc5ada6d6dbe7e9cdda681e58ee4f9572d4ff8
BrunoViet <123159709+BrunoViet@users.noreply.github.com>
2023-02-14 10:14:29 +0700

    hello

--- controller/deleteuser.js
+++ controller/deleteuser.js
@@ -3,5 +3,6 @@ let deleteuser = async (req, res) => {
     let userId = req.body.userId;
     await pool.execute('delete from users where id=?', [userId])
     return res.redirect('/booking-new');
+    console.log('check');
 }
 export default deleteuser;
